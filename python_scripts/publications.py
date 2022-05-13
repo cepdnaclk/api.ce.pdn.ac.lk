765908d9422fc4cb37a8ac68c51a2f29622c79e8
NuwanJ <nuwanjaliyagoda@gmail.com>
2022-05-13 13:32:47 +0530

    Implemented a basic publication API based on Google Form data.

---
+++ python_scripts/publications.py
@@ -0,0 +1,134 @@
+# REQUIREMENTS ------------
+# pip install requests
+# -------------------------
+
+# TODO:
+# No validation done by assume everything is ok,
+# But better to write validation logic too
+
+import requests
+import json
+import os
+
+from utility import getStudent
+
+# Where the API is available
+apiBase = "http://api.ce.pdn.ac.lk"
+
+# Where this API locates
+apiIndex = 'https://api.ce.pdn.ac.lk/publications/v1'
+# apiIndex = 'http://localhost:4001/publications/v1'
+
+# Where the student data available
+studentSource = "../people/v1/students/all/index.json"
+
+# Where the staff data available
+staffSource = "../people/v1/staff/all/index.json"
+
+DEFAULT_PROFILE_IMAGE = "https://people.ce.pdn.ac.lk/images/students/default.jpg"
+
+# Gather Student API data
+student_file = open(studentSource)
+students = json.load(student_file)
+
+# Gather Staff API data
+staff_file = open(staffSource)
+staff = json.load(staff_file)
+
+
+# ------------------------------------------------------------------------------
+
+# Get a unique identifier for each publication from the DOI link
+def get_id_from_doi(doi):
+    doi_id = doi.replace("http://", "").replace("https://", "")
+    doi_id = doi_id.replace("doi.org/", "")
+    doi_id = doi_id.replace("doi:", "")
+    return doi_id
+
+# Write individual files for each publication
+def write_publication(data):
+    doi_id = get_id_from_doi(data['doi'])
+
+    if doi_id != '#':
+        filename = "../publications/v1/{0}/index.json".format(doi_id)
+        # print(filename)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, "w") as f:
+            f.write(json.dumps(data, indent = 4))
+
+# ------------------------------------------------------------------------------
+
+google_form_link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQkkmY3EMIGJamXqsR0eSku9hGxrVcq_5R1EdY_laqhajwsHkROj9XWYxUbNcM4yYjw4nrcRr5XpSZY/pub?output=tsv"
+
+pub_raw = requests.get(google_form_link, headers={'Cache-Control': 'no-cache'}).text.split("\n")
+publications = []
+
+# Source file index
+TIMESTAMP = 0
+EMAIL = 1
+TITLE = 2
+VENUE = 3
+YEAR = 4
+DOI = 5
+ABSTRACT = 6
+AUTHORS = 7
+AUTHOR_IDS = 8
+PDF = 9
+PRESENTATION = 10
+CODEBASE = 11  # new
+RESEARCH_GROUPS = 12
+TAGS = 13
+FUNDING = 14
+
+FIELD_COUNT = 15
+
+# Ship the header line
+for line in pub_raw[1:]:
+
+    pub_raw_data = line.replace('\r', '').split("\t")
+
+    if(len(pub_raw_data) != FIELD_COUNT):
+        print("Not supported: ", ','.join(pub_raw_data))
+        continue
+
+    authors = [x.strip() for x in pub_raw_data[AUTHORS].split(',')]
+    author_ids = [x.strip() for x in pub_raw_data[AUTHOR_IDS].split(',')]
+    research_groups = [x.strip() for x in pub_raw_data[RESEARCH_GROUPS].split(',')]
+    tags = [x.strip() for x in pub_raw_data[TAGS].split(',')]
+
+    author_info = []
+
+    for author in author_ids:
+        author_id = author.split('@')[0]
+
+        if author_id in students:
+            author_info.append(getStudent(apiBase, students, author_id))
+
+    pub_data = {
+        "title": pub_raw_data[TITLE],
+        "venue": pub_raw_data[VENUE],
+        "year": pub_raw_data[YEAR],
+        "abstract": pub_raw_data[ABSTRACT],
+        "authors": authors,
+        "author_ids": author_ids,
+        "author_info": author_info,
+        "doi": pub_raw_data[DOI],
+        "pdf": pub_raw_data[PDF],
+        "presentation": pub_raw_data[PRESENTATION],
+        "codebase": pub_raw_data[CODEBASE],
+        "research_groups": research_groups,
+        "tags": tags,
+        "funding": pub_raw_data[FUNDING],
+    }
+
+    # Write into an individual file
+    write_publication(pub_data)
+
+    # Append into all list
+    publications.append(pub_data)
+
+
+filename = "../publications/v1/all/index.json"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+with open(filename, "w") as f:
+    f.write(json.dumps(publications, indent = 4))
---
+++ python_scripts/utility.py
@@ -0,0 +1,51 @@
+
+# Get the student object
+
+def getStudent(apiBase, students_dict, eNumber):
+    student = {}
+    if eNumber in students_dict:
+        # Check with the details available in the student API
+        person_from_api = students_dict[eNumber]
+
+        # Select the best available name
+        if(person_from_api['name_with_initials'] != ""):
+            name = person_from_api["name_with_initials"]
+        elif(person_from_api['preferred_long_name'] != ""):
+            name = person_from_api["preferred_long_name"]
+        elif(person_from_api['full_name'] != ""):
+            name = person_from_api["full_name"]
+
+        # Construct and select the Email address
+        if 'emails' in person_from_api:
+            # Try faculty email first
+            faculty_email = person_from_api['emails']['faculty']
+            personal_email = person_from_api['emails']['personal']
+
+            if faculty_email['name'] != "":
+                email = faculty_email['name'] + '@' + faculty_email['domain']
+            elif personal_email['name'] != "":
+                email = personal_email['name'] + '@' + personal_email['domain']
+            else:
+                email = '#'
+
+        # Get the profile image or set a default one
+        if 'profile_image' in person_from_api:
+            if 'profile_image' in person_from_api:
+                profile_image = person_from_api['profile_image']
+            else:
+                profile_image = DEFAULT_PROFILE_IMAGE
+        else:
+            profile_image = DEFAULT_PROFILE_IMAGE
+
+        profile_url = person_from_api['profile_page'] if 'profile_page' in person_from_api else "#"
+        profile_api = apiBase + "/people/v1/students/" + eNumber.replace("E/", "E")
+
+        student = {
+            'name':name, 'email':email,
+            'profile_image':profile_image , 'profile_url':profile_url
+        }
+
+    else:
+        student = null
+
+    return student
