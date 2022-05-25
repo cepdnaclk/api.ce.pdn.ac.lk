a541bbb698108bab8e982efc8a332ccc13d90c69
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2022-05-25 08:31:25 +0530

    Publication API updates  (#11)
    * Workflow updated to facilitate GitHub Secrets
    
    * Workflow updates

--- python_scripts/publication_notifications.py
+++ python_scripts/publication_notifications.py
@@ -1,6 +1,3 @@
-# REQUIREMENTS ------------
-# pip install discordwebhook
-
 # TODO:
 # No validation done by assume everything is ok,
 # But better to write validation logic too
@@ -16,20 +13,22 @@ today = datetime.now()
 # The time threshold considered for a notification generation
 NOTIFICATION_THRESHOLD = 60*60*24
 
-ENDPOINT = "https://discord.com/api/webhooks/978648828565659678/yXTWwVlybPTrzdJ-zFQi_4e7iSrGcJMbcF-ocJ_fStbylGiPi5mSLwju2KlmabPQIZ4A"
+# Webhook URL is stored as a GitHub Secret, which will be loaded as a Environment Variable at runtime
+# https://github.com/cepdnaclk/api.ce.pdn.ac.lk/settings/secrets/actions
+ENDPOINT = os.environ['discord_webhook']
 
 # ------------------------------------------------------------------------------
 
 def publish_discord(title, venue, year, authors, doi, tags):
     data = {
         "content": "A new research publication !",
-        "username": "api.ce.pdn.ac.lk",
+        "username": "CE Department Bot",
         "avatar_url": "https://api.ce.pdn.ac.lk/assets/img/bot_logo.png",
         "embeds": []
     }
     tag_string = ", ".join(tags)
     authors_string = ", ".join(authors)
-    venue_string = venue + "," + year
+    venue_string = venue + ", " + year
 
     embed = {"title": title, "color": "16735232", "fields": []}
     embed['fields'].append({"name": "Published at", "value": venue_string})
@@ -38,10 +37,16 @@ def publish_discord(title, venue, year, authors, doi, tags):
     embed['fields'].append({"name": "Tags", "value": tag_string})
 
     data['embeds'].append(embed)
-    print(json.dumps(data, indent = 4))
+    # print(json.dumps(data, indent = 4))
     response = requests.post(ENDPOINT, json=data)
-    print(response.status_code)
-    print(response.content)
+    # print(response.status_code)
+    # print(response.content)
+
+    if (response.status_code == 204):
+        print("\t Success")
+    else:
+        print("\t Error ! ", response.status_code, )
+        print(response.content)
 
 # ------------------------------------------------------------------------------
 
@@ -62,7 +67,7 @@ with open(publications_url, 'r') as f:
         tags = pub['tags']
 
         duration = today - created_time
-        print(title, duration, duration.total_seconds())
+        print("\n>> ", title, duration, duration.total_seconds())
 
         # This is a temp line for testing
         # publish_discord(title, venue, year, authors, doi, tags)
