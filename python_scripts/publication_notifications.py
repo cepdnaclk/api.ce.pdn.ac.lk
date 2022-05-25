90e012a8cfe6fe200b516b831cea5a779eeba589
NuwanJ <nuwanjaliyagoda@gmail.com>
2022-05-25 08:21:16 +0530

    Workflow updated to facilitate GitHub Secrets

--- python_scripts/publication_notifications.py
+++ python_scripts/publication_notifications.py
@@ -1,6 +1,3 @@
-# REQUIREMENTS ------------
-# pip install discordwebhook
-
 # TODO:
 # No validation done by assume everything is ok,
 # But better to write validation logic too
@@ -16,20 +13,23 @@ today = datetime.now()
 # The time threshold considered for a notification generation
 NOTIFICATION_THRESHOLD = 60*60*24
 
-ENDPOINT = "https://discord.com/api/webhooks/978648828565659678/yXTWwVlybPTrzdJ-zFQi_4e7iSrGcJMbcF-ocJ_fStbylGiPi5mSLwju2KlmabPQIZ4A"
+ENDPOINT = os.environ['discord_webhook']
+
+# REM: This is a temporary one
+print(ENDPOINT)
 
 # ------------------------------------------------------------------------------
 
 def publish_discord(title, venue, year, authors, doi, tags):
     data = {
         "content": "A new research publication !",
-        "username": "api.ce.pdn.ac.lk",
+        "username": "CE Department Bot",
         "avatar_url": "https://api.ce.pdn.ac.lk/assets/img/bot_logo.png",
         "embeds": []
     }
     tag_string = ", ".join(tags)
     authors_string = ", ".join(authors)
-    venue_string = venue + "," + year
+    venue_string = venue + ", " + year
 
     embed = {"title": title, "color": "16735232", "fields": []}
     embed['fields'].append({"name": "Published at", "value": venue_string})
@@ -62,7 +62,7 @@ with open(publications_url, 'r') as f:
         tags = pub['tags']
 
         duration = today - created_time
-        print(title, duration, duration.total_seconds())
+        print(">> ", title, duration, duration.total_seconds(), '\n')
 
         # This is a temp line for testing
         # publish_discord(title, venue, year, authors, doi, tags)
